a000c1c701b2ff12b06e7b8e122d055af25f1a27
MilesJohnson34 <milesivyjohnson@gmail.com>
2024-12-28 12:54:13 -0800

    hopefully this makes it lag less

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("sonic3scene","sonic3scene.mp4",.15,1,10)
+    encoder.encode("TheaterIntro","TheaterIntro.mp4",.2,.8,10)
 
 
 if __name__== "__main__":
